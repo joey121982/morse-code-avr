eda3b3867084253e53c7c6efef26ce116b8a2aed
Bratescu Andrei-Seren <121115397+joey121982@users.noreply.github.com>
2026-05-10 21:14:18 +0300

    small fix

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ uint8_t sos_state = 0;
 char transmit_buf[128];                  // transmission buffer
 char* transmit_buf_ptr = transmit_buf;   // current position in buffer
 
-char* receive_buf[128];                  // receive buffer, used only in timer0
+char  receive_buf[128];                  // receive buffer, used only in timer0
 
 void main(void) {
     setup();
